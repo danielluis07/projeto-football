f891dd7b5ded27e1ad4f1ec236d45851403756a8
danielluis07 <bombdaniel@hotmail.com>
2023-05-30 09:11:42 -0300

    design page principal

--- app/(website)/page.tsx
+++ app/(website)/page.tsx
@@ -1,11 +1,17 @@
 import AuthForm from "./components/AuthForm";
 
-export default function Home() {
+const Home = () => {
   return (
-    <main>
-      <div>
-        <AuthForm />
+    <div className="bg-[url('../public/images/gabigol.jpg')] bg-cover flex min-h-full flex-col justify-center py-12 sm:px-6 lg:px-8">
+      <div className="sm:mx-auto sm:w-full sm:max-w-md">
+        <div className="w-[150px] h-[150px] mx-auto"></div>
+        <h2 className="mt-6 text-white text-center text-3xl font-bold tracking-tight cursor-default">
+          Meu Time
+        </h2>
       </div>
-    </main>
+      <AuthForm />
+    </div>
   );
-}
+};
+
+export default Home;
